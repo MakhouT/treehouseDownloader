6856788cdc6e22f2e7434b583572e41f371c68b4
unknown <makhout.nabil@gmail.com>
2016-01-11 21:51:54 +0000

    Added the styling and removed the logs

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var router = require("./router.js");
 var http = require("http");
 
+//Create a basic server
 http.createServer(function(request, response){
 	router.home(request, response);
-	//router.download(request, response);
 }).listen(8080, "127.0.0.1");
 console.log("Server up and running on 127.0.0.1:8080");
